94ff966bd9828c7df0eec88c4f146ea8b25e4dd7
ozancansel <ozan.cansel@hazelcast.com>
2023-01-16 09:43:36 +0300

    CompactMix tests are added. It contains mostly tests for uncovered lines.

--- hazelcast/test/src/compact_test.cpp
+++ hazelcast/test/src/compact_test.cpp
@@ -23,3 +23,4 @@
 #include "compact/compact_schema_replication_stress_test.h"
 #include "compact/compact_schema_fetch_on_read.h"
 #include "compact/compact_read_write_integration_test.h"
+#include "compact/compact_mix.h"
